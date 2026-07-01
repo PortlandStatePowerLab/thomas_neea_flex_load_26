7fe183264ed78592e27dcd194bd5c866f55282ce
t-metzler <tmetzler@pdx.edu>
2026-06-30 21:40:01 -0700

    AC data & plotting

---
+++ AC/AC_C1_parse_OCHRE_data_final.py
@@ -0,0 +1,114 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 24 14:46:47 2025
+
+@author: Joe_admin
+@modified by: Jeff Dinsmore
+@modified date: 12/14/2025
+@modified by: Thomas Metzler
+@modified date: 6/17/2026
+"""
+
+
+import pandas as pd
+from datetime import datetime
+import csv
+import os
+
+# Converts the datetime information in the HEMS data to usable datetimes
+def convert_custom_datetime(series):
+    return series.apply(lambda x: datetime.strptime(x, "%d/%m/%Y %H:%M"))
+
+
+############################################################################
+#                           Enter inputs here                              #
+############################################################################
+
+# enter in the input and output file names. 
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+fl_dir = os.path.dirname(script_dir)  
+working_dir = os.path.dirname(fl_dir)  
+
+input_file_root = "AC_Test"
+
+input_file_name1 = input_file_root + "_baseline"
+input_file_name2 = input_file_root + "_controlled"
+input_file_1  = os.path.join(working_dir, input_file_name1 +".csv")
+input_file_2  = os.path.join(working_dir, input_file_name2 +".csv")
+
+output_append_ACpower = "_AC_power"
+output_file_name1 = input_file_name1 + output_append_ACpower + ".csv"
+output_file_name2 = input_file_name2 + output_append_ACpower + ".csv"
+folder_path = os.path.join(working_dir, "Ready_data", input_file_root)
+output_file_1 = os.path.join(working_dir, "Ready_data", input_file_root, output_file_name1)
+output_file_2 = os.path.join(working_dir, "Ready_data", input_file_root, output_file_name2)
+
+output_append_totpower = "_total_power"
+output_file_name3 = input_file_name1 + output_append_totpower + ".csv"
+output_file_name4 = input_file_name2 + output_append_totpower + ".csv"
+output_file_3 = os.path.join(working_dir, "Ready_data", input_file_root, output_file_name3)
+output_file_4 = os.path.join(working_dir, "Ready_data", input_file_root, output_file_name4)
+
+
+############################################################################
+#                             Create Folder                                #
+############################################################################
+
+# Check for file path and create if does not exist
+if not os.path.exists(folder_path):
+    os.makedirs(folder_path) # os.mkdir() creates only one level; os.makedirs() creates intermediate parents
+    print(f"Directory created: {folder_path}")
+else:
+    print(f"Directory already exists: {folder_path}")
+
+############################################################################
+#                             Program Start                                #
+############################################################################
+
+def process_data(input_file, output_file, wanted_col):
+    # read data 
+    df = pd.read_csv(input_file)
+
+    # remove any NAN values that will mess up the datetime conversion. 
+    df = df.dropna(axis=0)
+
+    reader = csv.DictReader(input_file)
+
+    # Access the columns attribute
+    print("Column Titles:")
+    # The .columns attribute returns an Index object, which can be printed directly or iterated
+    for col in df.columns:
+        print(f"- {col}")
+    # convert time column to a usable datetime fomat
+    df['time'] = pd.to_datetime(df['Time'], errors='coerce')
+    #df['time'] = convert_custom_datetime(df['Time'])
+
+    # Create column that contains hour and minute data
+    df['hr_min'] = df['time'].dt.strftime('%H:%M')
+
+    cols = ['Time',
+        "Total Electric Power (kW)",
+        "Total Electric Energy (kWh)",
+        "HVAC Cooling Electric Power (kW)",
+        "HVAC Cooling COP (-)",
+        "Temperature - Indoor (C)",
+        "HVAC Heating Electric Power (kW)" ]
+
+    #identify unwanted columns to drop
+    unwanted_cols = cols.copy()
+    unwanted_cols.remove(wanted_col)
+
+    # drop unwanted columns
+    df = df.drop(unwanted_cols, axis=1)
+
+    # pivot the table
+    df_pivot = df.pivot_table(index = 'Home', columns = 'hr_min', values = wanted_col)
+
+    # write data to csv
+    df_pivot.to_csv(output_file, index=True)
+
+process_data(input_file_1, output_file_1, 'HVAC Cooling Electric Power (kW)')
+process_data(input_file_2, output_file_2, 'HVAC Cooling Electric Power (kW)')
+process_data(input_file_1, output_file_3, 'Total Electric Power (kW)')
+process_data(input_file_2, output_file_4, 'Total Electric Power (kW)')
---
+++ AC/AC_C2_Plot_Totpower_WHpower.py
@@ -0,0 +1,120 @@
+"""
+#Author: Thomas Metzler
+#6/22/2026
+
+#Creates plots for the average water heater and total household power consumption, comparing baseline and controlled
+#Works for HPWH 
+"""
+
+
+import pandas as pd
+import os
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+
+
+#Copy path naming from HPWH_parse_OCHRE_data_final.py for consistency
+script_dir = os.path.dirname(os.path.abspath(__file__))
+fl_dir = os.path.dirname(script_dir) 
+working_dir = os.path.dirname(fl_dir)  
+
+input_file_root = "AC_Test"
+
+input_file_name1 = input_file_root + "_baseline"
+input_file_name2 = input_file_root + "_controlled"
+input_file_1  = os.path.join(working_dir, input_file_name1 +".csv")
+input_file_2  = os.path.join(working_dir, input_file_name2 +".csv")
+
+output_append_ACpower = "_AC_power"
+output_file_name1 = input_file_name1 + output_append_ACpower + ".csv"
+output_file_name2 = input_file_name2 + output_append_ACpower + ".csv"
+folder_path = os.path.join(working_dir, "Ready_data", input_file_root)
+output_file_1 = os.path.join(working_dir, "Ready_data", input_file_root,output_file_name1)
+output_file_2 = os.path.join(working_dir, "Ready_data", input_file_root, output_file_name2)
+
+output_append_totpower = "_total_power"
+output_file_name3 = input_file_name1 + output_append_totpower + ".csv"
+output_file_name4 = input_file_name2 + output_append_totpower + ".csv"
+output_file_3 = os.path.join(working_dir, "Ready_data", input_file_root, output_file_name3)
+output_file_4 = os.path.join(working_dir, "Ready_data", input_file_root, output_file_name4)
+
+photo_file_1 = os.path.join(working_dir, "Ready_data", input_file_root, input_file_root + "_WH_power_plot.png")
+photo_file_2 = os.path.join(working_dir, "Ready_data", input_file_root, input_file_root + "_Total_power_plot.png")
+
+
+#Saves the average of each column as a new row
+def save_avg(file):
+    # 1. Read the CSV file into a DataFrame
+    df = pd.read_csv(file)
+
+    # 2. Calculate the mean for numeric columns
+    averages = df.mean(numeric_only=True)
+
+    # 3. Append the averages as a new row (using 'Average' as the row label/index)
+    df.loc['Average'] = averages
+
+    # 4. Save back to a CSV file
+    df.to_csv(file, index=False)
+
+#plot the data and save the plot
+def plot_data(baseline_file, controlled_file, title, photo_file, ax):
+    # Load the CSV file
+    df_base = pd.read_csv(baseline_file, index_col=0)  # Assuming the first column is the index
+    df_con= pd.read_csv(controlled_file, index_col=0)  # Assuming the first column is the index
+
+    # Extract the first row (index 0) and last row (index -1)
+    # .iloc[[0, -1]] selects both rows into a new DataFrame
+    subset_df_base = df_base.iloc[[0, -1]]
+    subset_df_con = df_con.iloc[[0, -1]]
+
+    #transpose data to have time as columns and power as rows
+    transposed_df_base = subset_df_base.iloc[1,:]
+    transposed_df_con = subset_df_con.iloc[1,:]
+    
+    #Switch to dataframe and reset index to have time as a column
+    df_base = pd.DataFrame (transposed_df_base)
+    df_con = pd.DataFrame (transposed_df_con)
+    
+    df_base.index = df_base.index.str.replace('Home', 'Time')
+    df_con.index = df_con.index.str.replace('Home', 'Time')
+
+    df_base = df_base.reset_index ()
+    # change columns names:
+    df_base.columns = ['Time', 'baseline']
+
+    df_con = df_con.reset_index ()
+    # change columns names:
+    df_con.columns = ['Time', 'controlled']
+
+    # convert time column to a usable datetime fomat
+    df_base['Time'] = pd.to_datetime(df_base['Time'], errors='coerce').dt.strftime('%H:%M')
+    df_con['Time'] = pd.to_datetime(df_con['Time'], errors='coerce').dt.strftime('%H:%M')
+
+    #plot
+    fig, ax = plt.subplots(figsize=(8, 5))
+    ax.plot (df_base['Time'], df_base['baseline'], label='baseline', color='blue', linestyle='-')
+    ax.plot (df_con['Time'], df_con['controlled'], label='controlled', color='orange', linestyle='--')
+
+    # Customize and display
+    ax.set_title(title)
+    ax.set_xlabel('Time')
+    ax.set_ylabel('Power (kW)')
+    ax.legend() # Displays labels properly
+    # ax.set_xlim(0, 24)
+    # ax.set_xticks([0,6,12,18,24])
+
+    ax.xaxis.set_major_locator(ticker.MaxNLocator(nbins=6))  # Limit ticks
+
+    plt.savefig(photo_file, dpi=300, bbox_inches='tight')  # Save the figure
+
+save_avg(output_file_1)
+save_avg(output_file_2)
+plot_data(output_file_1, output_file_2, 'Average Power Consumption per AC System', photo_file_1, "ax1")
+
+save_avg(output_file_3)
+save_avg(output_file_4)
+plot_data(output_file_3, output_file_4, 'Average Total Power Consumption per Household', photo_file_2, "ax2")
+
+#Show plot at the end so it doesn't overwrite the previous plot
+plt.show()
+
