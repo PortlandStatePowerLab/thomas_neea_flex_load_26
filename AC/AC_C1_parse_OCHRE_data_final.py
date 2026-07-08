3faeb044b537f273ac035f20d1b43b12504235c5
t-metzler <tmetzler@pdx.edu>
2026-07-08 11:44:36 -0700

    Loadup vs shed

--- AC/AC_C1_parse_OCHRE_data_final.py
+++ AC/AC_C1_parse_OCHRE_data_final.py
@@ -30,7 +30,7 @@ script_dir = os.path.dirname(os.path.abspath(__file__))
 fl_dir = os.path.dirname(script_dir)  
 working_dir = os.path.dirname(fl_dir)  
 
-input_file_root = "AC_Test_Real_Time_VPP_Fleetstates"
+input_file_root = "AC_Test_LOADUP_VS_SHED"
 
 input_file_name1 = input_file_root + "_baseline"
 input_file_name2 = input_file_root + "_controlled"
